bbd51e3b203aeebbe6f13bc4717deb1a60154908
David Morton <davidlmorton@gmail.com>
2013-12-10 13:56:30 -0600

    Update properties function to support is_many queries
    Change-Id: I93e9830704ca9a0001f3c8c41ec58df016b54c81

--- lib/Procera/WorkflowCompatibility/FakePropertyHolder.pm
+++ lib/Procera/WorkflowCompatibility/FakePropertyHolder.pm
@@ -22,6 +22,69 @@ has 'param_properties' => (
     default => sub {[]},
 );
 
+sub _input_properties_set {
+    my $self = shift;
+
+    return _property_name_set(@{$self->input_properties});
+}
+
+sub _output_properties_set {
+    my $self = shift;
+
+    return _property_name_set(@{$self->output_properties});
+}
+
+sub _param_properties_set {
+    my $self = shift;
+
+    return _property_name_set(@{$self->param_properties});
+}
+
+sub _all_properties_set {
+    my $self = shift;
+
+    return _property_name_set(
+        @{$self->input_properties},
+        @{$self->output_properties},
+        @{$self->param_properties},
+    );
+}
+
+sub _is_array_properties_set {
+    my $self = shift;
+
+    return _property_name_set(
+        grep {$_->is_array} @{$self->input_properties},
+        grep {$_->is_array} @{$self->output_properties},
+        grep {$_->is_array} @{$self->param_properties},
+    );
+}
+
+sub _property_name_set {
+    return Set::Scalar->new(map {$_->property_name} @_);
+}
+
+sub _lookup_property {
+    my ($self, $property_name) = @_;
+
+    unless (exists($self->_property_hash->{$property_name})) {
+        confess "Attempt to lookup unknown property named ($property_name)";
+    }
+    return $self->_property_hash->{$property_name};
+}
+
+sub _property_hash {
+    my $self = shift;
+
+    my %result;
+    for my $property (
+            @{$self->input_properties},
+            @{$self->output_properties},
+            @{$self->param_properties}) {
+        $result{$property->property_name} = $property;
+    }
+    return \%result;
+}
 
 sub properties {
     my $self = shift;
@@ -63,26 +126,27 @@ sub properties {
         confess "Moose-WorkflowCompatibility doesn't support parallel_by";
     }
 
-    if ($params{is_many}) {
-        return;  # For now we don't support this.
-    }
-
-    my @result;
+    my $result;
     if ($params{is_input}) {
-        push @result, @{$self->input_properties};
+        $result = $self->_input_properties_set;
     } elsif ($params{is_output}) {
-        push @result, @{$self->output_properties};
+        $result = $self->_output_properties_set;
     } elsif ($params{is_param}) {
-        push @result, @{$self->param_properties};
+        $result = $self->_param_properties_set;
     } else {
-        push @result, @{$self->input_properties};
-        push @result, @{$self->output_properties};
-        push @result, @{$self->param_properties};
+        $result = $self->_all_properties_set;
     }
 
     if (my $property_name = $params{property_name}) {
-        @result = grep {$_->property_name eq $property_name} @result;
+        $result = $result->intersection(Set::Scalar->new($property_name));
     }
+
+    if ($params{is_many}) {
+        $result = $result->intersection($self->_is_array_properties_set);
+    }
+
+
+    my @result = map {$self->_lookup_property($_)} $result->members;
     if (wantarray) {
         return @result;
     } else {
