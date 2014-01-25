9f94cd7213d66b1d8b55c35e73a97b5af3b912cc
David Morton <davidlmorton@gmail.com>
2014-01-25 11:39:44 -0600

    Use Params::Validate to ensure positional params are passed as expected.

--- lib/Procera/Persistence/Memory.pm
+++ lib/Procera/Persistence/Memory.pm
@@ -3,6 +3,8 @@ package Procera::Persistence::Memory;
 use MooseX::Singleton;
 use warnings FATAL => 'all';
 
+use Params::Validate qw(validate_pos);
+
 with 'Procera::Persistence::Detail::Role';
 
 
@@ -25,7 +27,7 @@ has '_counter' => (
 );
 
 sub create_process {
-    my ($self, $content) = @_;
+    my ($self, $content) = validate_pos(@_, 1, 1);
 
     return $self->_get_label('processes');
 }
@@ -41,7 +43,7 @@ sub get_result {
 }
 
 sub create_fileset {
-    my ($self, $content) = @_;
+    my ($self, $content) = validate_pos(@_, 1, 1);
 
     my $fileset_label = $self->_get_label('filesets');
     $self->_allocations->{$fileset_label} = $content->{allocations}->[0];
@@ -66,19 +68,19 @@ sub add_step_to_process {
 }
 
 sub get_file {
-    my ($self, $url) = @_;
+    my ($self, $url) = validate_pos(@_, 1, 1);
 
     return $self->_files->{$url};
 }
 
 sub get_allocation_id_for_fileset {
-    my ($self, $url) = @_;
+    my ($self, $url) = validate_pos(@_, 1, 1);
 
     return $self->_allocations->{$url};
 }
 
 sub _get_label {
-    my ($self, $base_label) = @_;
+    my ($self, $base_label) = validate_pos(@_, 1, 1);
 
     my $label = sprintf("/v1/%s/%d", $base_label, $self->_counter);
     $self->_counter($self->_counter + 1);
