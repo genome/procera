6b620b83f531131db3a2cacab6b5b3e3b50d6fb9
David Morton <davidlmorton@gmail.com>
2014-02-08 11:57:26 -0600

    Add Procera::Curator and some tests
    The Curator is responsible for determining information
    useful to other parties such as the
    inputs/params(non_contextual)/outputs for tools and processes.

---
+++ t/Curator/TestHelper.pm
@@ -0,0 +1,80 @@
+package Curator::TestHelper;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Data::Dump qw(pp);
+use File::Copy qw();
+use File::Slurp qw();
+use File::Spec qw();
+use File::Temp qw();
+use Carp qw(confess);
+use Test::More;
+
+use Procera::Curator;
+
+my $SOURCE_PATH = 'Subject';
+
+sub run {
+    my (undef, $test_file) = caller;
+    my ($junk, $test_dir) = File::Basename::fileparse($test_file);
+
+    unshift @INC, File::Spec->join($test_dir, 'perl');
+    my $old_gms_path = $ENV{GMSPATH};
+    $ENV{GMSPATH} = File::Spec->join($test_dir, 'definitions');
+
+    curate($test_dir, $SOURCE_PATH);
+
+    $ENV{GMSPATH} = $old_gms_path;
+    shift @INC;
+
+    done_testing();
+}
+
+sub curate {
+    my ($test_dir, $source_path) = @_;
+
+    my $curator = Procera::Curator->new(source_path => $source_path);
+    my $created_report = write_report($curator);
+    my $expected_report = File::Spec->join($test_dir, 'expected_report.txt');
+    diff_files($created_report, $expected_report, "Found the report as expected");
+}
+
+sub write_report {
+    my $curator = shift;
+
+    my $output_filename = File::Spec->join(File::Temp::tempdir(CLEANUP => 1),
+        'report.txt');
+    my $output_fh = IO::File->new($output_filename, 'w');
+    printf $output_fh "Inputs:\n%s\nParams:\n%s\nOutputs:\n%s\n",
+        pp($curator->inputs),
+        pp($curator->params),
+        pp($curator->outputs);
+    $output_fh->close();
+    return $output_filename;
+}
+
+sub diff_files {
+    my ($got, $expected, $statement) = @_;
+
+    my $got_text = File::Slurp::read_file($got);
+    my $expected_text = File::Slurp::read_file($expected);
+    is_deeply($got_text, $expected_text, $statement);
+
+    update_test_data($got, $expected);
+}
+
+sub update_test_data {
+    my ($got, $expected) = @_;
+
+    if ($ENV{UPDATE_TEST_DATA}) {
+        my $result = File::Copy::copy($got, $expected);
+        if ($result) {
+            print "Copied file ($got) to ($expected)\n";
+        } else {
+            confess "failed to copy file ($got) to ($expected)";
+        }
+    }
+}
+
+1;
